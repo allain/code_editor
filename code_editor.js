d54f3ff5a6c72c8b532a6ef9cd1de2eef79a0765
Allain Lalonde <allain@machete.ca>
2011-02-21 16:41:49 -0600

    Making module conform to Drupal coding standards

--- code_editor.js
+++ code_editor.js
@@ -1,12 +1,6 @@
+// $Id$
+
 $(function() {
   $(".CodeMirror-wrapping").parents(".form-item:first").find(".grippie").hide();
-  $("h2:contains('Edit Code')").hide();
-  $("#code-editor-edit-form").submit(function(e) {
-    if ($(".syntax-error").length) {
-      alert("You can't save when there syntax errors.");
-      
-      return false; 
-    }  
-  });
-  
+  $("h2:contains('Edit Code')").hide(); 
 });
